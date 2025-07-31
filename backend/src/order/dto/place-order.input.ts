520f827f525bce54d26c869ff7e1301d29d5b018
tommyle1310 <tommyle1310@gmail.com>
2025-07-31 16:47:36 +0700

    remove duplicated Ordertype enum registration

--- backend/src/order/dto/place-order.input.ts
+++ backend/src/order/dto/place-order.input.ts
@@ -1,11 +1,7 @@
 import { InputType, Field, registerEnumType } from '@nestjs/graphql';
 import { OrderSide } from '../enums/order-side.enum';
-import { OrderType } from '@prisma/client';
+import { OrderType } from '../enums/order-type.enum';
 import { TimeInForce } from '../enums/time-in-force.enum';
-
-registerEnumType(OrderType, {
-  name: 'OrderType',
-});
 
 registerEnumType(TimeInForce, {
   name: 'TimeInForce',
